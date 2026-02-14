3b7e3c19d81862af99126b4f418392b4e1b20221
Rahul gowda <gowdarahul185@gmail.com>
2026-02-14 14:35:56 +0530

    9

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Express Frontend Running with Docker');
+  res.send('Express Frontend Running!');
 });
 
-app.listen(3000, () => {
+app.listen(3000, '0.0.0.0', () => {
   console.log('Server running on port 3000');
 });
